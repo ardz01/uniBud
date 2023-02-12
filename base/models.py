f9ebdf53c18f35e7e03a6a4c266459dc46c493da
ardz01 <79152920+ardz01@users.noreply.github.com>
2023-02-12 18:30:43 +0000

    search
    added search functionality

--- base/models.py
+++ base/models.py
@@ -25,6 +25,9 @@ class Room(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
 
+    class Meta:
+        ordering = ['-updated', '-created']
+
 
     def __str__(self):
         return self.name
