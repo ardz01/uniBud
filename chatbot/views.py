ae582dc6dbc73c3caf9525f4b5b409ca47f109cd
ardz01 <79152920+ardz01@users.noreply.github.com>
2023-04-02 19:15:59 +0100

    rules for chatbot

--- chatbot/views.py
+++ chatbot/views.py
@@ -42,7 +42,32 @@ def process_question(question):
     if 'events' in tokens:
         return "There are many events happening on campus throughout the year, such as guest lectures, workshops, and cultural events. Check out our events calendar on the website for more information."
 
-    # Add more rules here for additional functionality
+    # Financial aid
+    if 'financial' in tokens and ('aid' in tokens or 'help' in tokens or 'assistance' in tokens):
+        return "Financial aid is available to eligible students. You can apply for scholarships, grants, work-study programs, and loans. Visit our financial aid office or check our website for more information."
+
+    # Majors
+    if 'major' in tokens or 'majors' in tokens:
+        return "Our college offers a variety of majors, including Computer Science, Engineering, Business, Humanities, and Social Sciences. You can find a full list of majors on our website or by contacting the admissions office."
+
+    # Sports
+    if 'sports' in tokens or 'athletics' in tokens:
+        return "We have a variety of sports teams and athletic programs on campus, including basketball, football, soccer, and swimming. You can find more information about our sports programs on our website or by visiting the athletics department."
+
+    # Library
+    if 'library' in tokens or 'resources' in tokens:
+        return "Our college library provides access to a wide range of resources, including books, journals, databases, and study spaces. You can find more information about the library and its services on our website."
+
+    # Housing
+    if 'housing' in tokens or 'dorm' in tokens or 'dormitory' in tokens:
+        return "We offer a variety of housing options for students, including dormitories and apartments. You can find more information about housing, including application deadlines and fees, on our website or by contacting the housing office."
+
+    # Registration
+    if 'registration' in tokens or 'enrollment' in tokens:
+        return "Registration for classes typically opens a few months before the start of each semester. You can find information about registration dates and deadlines on our website or by contacting the registrar's office."
+
+# Add more rules here for additional functionality
+
 
     return "I am a simple chatbot, and I don't understand your question yet."
 
